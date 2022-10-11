5091fac43e7e8ccaf1d016c4c394eccbf4b4583c
Lorena Pinzón <lorena5pinzon@gmail.com>
2022-10-10 19:07:21 -0500

    Implement internationalization

--- src/main/java/com/lore/rest/demorestwebservice/service/HelloWorldController.java
+++ src/main/java/com/lore/rest/demorestwebservice/service/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.lore.rest.demorestwebservice.service;
 
 import com.lore.rest.demorestwebservice.dto.HelloWorld;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+    private final MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "/helloWorld")
     public String HelloWorls(){
@@ -21,5 +30,11 @@ public class HelloWorldController {
     @GetMapping(path = "/helloWorldBeanVariable/{name}")
     public HelloWorld HelloWorlsBeanVariable(@PathVariable String name){
         return new HelloWorld("HelloWorld, " + name);
+    }
+
+    @GetMapping(path = "/helloWorldInternationalized")
+    public String helloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
